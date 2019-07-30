7336cd69178b6441937d8b680d63c2bec48a6b24
Samuel Degueldre <samuelfdegueldre@gmail.com>
2019-07-30 10:01:37 +0200

    Make topic into class, change root id to 'root'

--- src/services/API.tsx
+++ src/services/API.tsx
@@ -1,23 +1,26 @@
-export type Topic = {
-  name: string,
-  subTopics?: Array<string>
+export class Topic {
+  constructor(
+    public name: string,
+    public subTopics?: Array<string>
+  ){ };
 };
 
 // Database stub
 const topics = new Map<string, Topic>([
-  ['topic list', {name: 'Topic list', subTopics: ['1', '2', 'uuid']}],
-  ['1', {name: 'Topic 1', subTopics: ['uuid', '2']}],
-  ['2', {name: 'Topic 2'}],
-  ['uuid', {name: 'A random topic'}],
+  ['root', new Topic('Topic list', ['1', '2', 'uuid'])],
+  ['1', new Topic('Topic 1', ['uuid', '2'])],
+  ['2', new Topic('Topic 2')],
+  ['uuid', new Topic('A random topic')],
 ]);
 
 export default ({
   getTopic: (id: string): Topic => {
     const topic = topics.get(id);
+    console.log(Array.from(topics));
     if(!topic)
       throw new Error(`Could not render non-existant topic with id "${id}"`);
     return topic;
   }
 });
 
-export const ROOT_ID = 'topic list';
+export const ROOT_ID = 'root';
