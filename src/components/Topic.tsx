9d761014636a07a5d6ab7d8f43df77af290edb9d
Samuel Degueldre <samuelfdegueldre@gmail.com>
2019-08-23 13:31:54 +0200

    [ADD] Add keyboard navigation to search bar

--- src/components/Topic.tsx
+++ src/components/Topic.tsx
@@ -47,42 +47,49 @@ export default ((props) => {
     }
   }
 
+  const contents = topic.contents.filter(c => c.toLowerCase().includes(searchQuery.toLowerCase()));
+  const linkedTopics = topic.linkedTopics
+    .map(id => ({id, name: API.getTopic(id).name}))
+    .filter(t => t.name.toLowerCase().includes(searchQuery.toLowerCase()));
+
   return (
     <>
     <h2 className="text-center d-block">{topic.name}</h2>
     <div className="topic container px-5 py-4 my-4 d-flex flex-column">
-    <label className="px-0 col-12 col-md-6 ml-auto mb-3 d-flex flex-row align-items-center">
-      Search: <input className="ml-3 flex-grow-1" type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}></input>
-    </label>
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      if(linkedTopics.length == 1){
+        props.history.push(`/${linkedTopics[0].id}`);
+        setSearchQuery('');
+      }
+    }}>
+      <label className="px-0 col-12 col-md-6 ml-auto mb-3 d-flex flex-row align-items-center">
+        Search: <input className="ml-3 flex-grow-1" type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} autoFocus={true}></input>
+      </label>
+    </form>
       {topic.id !== ROOT_ID && <>
         <hr/>
         <div className="d-flex flex-column">
-          {topic.contents
-            .filter(c => c.toLowerCase().includes(searchQuery.toLowerCase()))
-            .map((content, contentId) => (
-                <div className="d-flex flex-row align-items-center flex-wrap" key={content}>
-                  <ContentBlock content={content} className="col-12 col-md-9"/>
-                  <button onClick={() => editContent(contentId)} className="btn btn-secondary ml-auto">Edit</button>
-                  <button onClick={() => removeContent(contentId)} className="btn btn-danger">Delete</button>
-                </div>
-              )
+          {contents.map((content, contentId) => (
+              <div className="d-flex flex-row align-items-center flex-wrap" key={content}>
+                <ContentBlock content={content} className="col-12 col-md-9"/>
+                <button onClick={() => editContent(contentId)} className="btn btn-secondary ml-auto">Edit</button>
+                <button onClick={() => removeContent(contentId)} className="btn btn-danger">Delete</button>
+              </div>
             )
-          }
+          )}
           <button onClick={addContent} className="btn btn-primary mx-auto">Add content</button>
         </div>
         <hr/>
         <h2 className="text-center">Related topics:</h2>
       </>}
       <div className="d-flex flex-column">
-        {topic.linkedTopics
-          .map(id => ({id, name: API.getTopic(id).name}))
-          .filter(t => t.name.toLowerCase().includes(searchQuery.toLowerCase()))
-          .map(t => (
-            <div className="d-flex flex-row align-items-center" key={t.id}>
-              <button onClick={() => props.history.push(`/${t.id}`)}>{t.name}</button>
-              <button onClick={() => removeTopic(t.id)} className="btn btn-danger ml-auto">Delete</button>
-            </div>)
-          )}
+        {linkedTopics.map(t => (
+          <div className="d-flex flex-row align-items-center" key={t.id}>
+            <button onClick={() => props.history.push(`/${t.id}`)}>{t.name}</button>
+            <button onClick={() => removeTopic(t.id)} className="btn btn-danger ml-auto">Delete</button>
+          </div>)
+        )}
         <button onClick={addTopic} className="btn btn-primary mx-auto">Add topic</button>
       </div>
     </div>
