b36de486404d2608780b32e8e66545d08e86b69f
Samuel Degueldre <samuelfdegueldre@gmail.com>
2019-08-23 12:05:22 +0200

    Make navbar fixed top and add a navbar placeholder so that the content of the page doesn't go behind it

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,16 @@ import { Link, RouteComponentProps } from 'react-router-dom';
 
 export default ((props) => {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    <>
+    // Navbar placeholder so that the fixed navbar doesn't let the content go behind it
+    <nav className="navbar navbar-expand-md">
+      <button className="navbar-brand btn btn-link mr-0">←</button>
+    </nav>
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
       <button className="navbar-brand btn btn-link mr-0" onClick={() => props.history.goBack()}>←</button>
       <button className="navbar-brand btn btn-link ml-0" onClick={() => props.history.goForward()}>→</button>
       <Link className="navbar-brand" to="/">Treeganizer</Link>
     </nav>
+    </>
   )
 }) as React.FC<RouteComponentProps<never>>;
